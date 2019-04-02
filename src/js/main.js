69dafc5cec1de2e09b915e2f4ac2c88ed10a749f
Robin Löffel <r.loeffel@outlook.com>
2019-04-02 12:16:19 +0200

    :rainbow: improve mobile layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
-import './polyfills';
-
 import jump from 'jump.js';
+import './polyfills';
 
 const navigation = document.querySelector('.header__navigation');
 const jumpLinks = document.querySelectorAll('[data-jump]');
+const scrollToNode = event => {
+    jump(event.target.dataset.jump);
+    navigation.classList.remove('header__navigation--translated');
+    event.preventDefault();
+};
 
 document.addEventListener('scroll', () => {
     if (!navigation.classList.contains('header__navigation--white') && window.scrollY > 0) {
@@ -16,13 +20,7 @@ document.addEventListener('scroll', () => {
 });
 
 [...jumpLinks].forEach(node => {
-    node.addEventListener('click', event => {
-        jump(event.target.dataset.jump);
-
-        navigation.classList.remove('header__navigation--translated');
-
-        event.preventDefault();
-    });
+    node.addEventListener('click', scrollToNode);
 });
 
 import './i18n';
--- src/js/mobileNav.js
+++ src/js/mobileNav.js
@@ -1,9 +1,7 @@
 const trigger = document.querySelector('.header__link--mobile-nav');
 const navigation = document.querySelector('.header__navigation');
-
 const toggleMobileNav = event => {
     navigation.classList.toggle('header__navigation--translated');
-
     event.preventDefault();
 };
 
