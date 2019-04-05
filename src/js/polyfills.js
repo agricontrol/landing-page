74274212acedb7e274a82c9581d7464bd922f8de
Robin Löffel <r.loeffel@outlook.com>
2019-04-05 13:07:19 +0200

    start fixing ie

--- src/js/polyfills.js
+++ src/js/polyfills.js
@@ -1,1 +1,18 @@
 import 'svgxuse';
+
+if (!('objectFit' in document.documentElement.style)) {
+    const images = document.querySelectorAll('[data-object-fit]');
+
+    [...images].forEach(image => {
+        const div = document.createElement('div');
+        div.style.backgroundImage = `url(${image.src})`;
+        div.style.backgroundSize = image.dataset.objectFit || 'cover';
+        div.style.backgroundPosition = image.dataset.objectPosition || 'center';
+        div.classList.add('object-fit-polyfilled');
+        image.classList.forEach(name => {
+            div.classList.add(name);
+        });
+        image.parentNode.insertBefore(div, image);
+        image.style.display = 'none';
+    });
+}
