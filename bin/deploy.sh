be189b0c468fd1bc78dd4d719ba0949da23e9458
Robin Löffel <robin.loeffel@unic.com>
2019-11-22 11:18:58 +0100

    finish up ci stuff hopefully

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "hey"
+tar -czvf page.tar.gz dist
+
+scp page.tar.gz "${1}@${2}:public_html"
+
+ssh agricont@agricontrol.app << EOF
+cd public_html
+rm -rf data css img js *.html *.txt .htaccess
+tar -xzvf page.tar.gz
+mv dist/* .
+mv dist/.htaccess .
+rm -rf page.tar.gz dist
+exit
+EOF
+
+rm page.tar.gz
