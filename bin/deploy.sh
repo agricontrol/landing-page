4152a1251b263c1006d1a95363803701c36a63d6
Robin Löffel <robin.loeffel@unic.com>
2019-11-22 13:04:03 +0100

    use variables everywhere

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,7 +4,7 @@ tar -czvf page.tar.gz dist
 
 scp page.tar.gz "${1}@${2}:public_html"
 
-ssh agricont@agricontrol.app << EOF
+ssh "${1}@${2}" << EOF
 cd public_html
 rm -rf data css img js *.html *.txt .htaccess
 tar -xzvf page.tar.gz
