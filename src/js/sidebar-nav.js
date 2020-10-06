f5a87a3d6e33b863fe3310d9de33f3a19e35a5b9
Robin Löffel <hi@robinloeffel.ch>
2020-10-06 10:46:38 +0200

    start translating faq page

--- src/js/sidebar-nav.js
+++ src/js/sidebar-nav.js
@@ -4,26 +4,42 @@ const aside = document.querySelector('[data-sidebar-nav=aside]');
 const content = document.querySelector('[data-sidebar-nav=content]');
 
 if (aside && content) {
-  const headings = content.querySelectorAll('.title');
+  document.addEventListener('mehrsprachigTranslated', () => {
+    aside.innerHTML = '';
 
-  const smoothScroll = event => {
-    event.preventDefault();
+    const headings = content.querySelectorAll('.title');
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        const navItem = aside.querySelector(`[data-sidebar-nav-target='${entry.target.dataset.sidebarNavId}'`);
 
-    const scrollTarget = content.querySelector(`[data-sidebar-nav-id='${event.target.dataset.sidebarNavTarget}']`);
-    jump(scrollTarget, {
-      offset: -120
+        if (entry.isIntersecting) {
+          navItem.classList.add('sidebar-link--visible');
+        } else {
+          navItem.classList.remove('sidebar-link--visible');
+        }
+      });
     });
-  };
 
-  headings.forEach((heading, i) => {
-    const anchor = document.createElement('a');
-    anchor.textContent = heading.textContent;
-    anchor.classList.add('link');
-    anchor.classList.add('sidebar-link');
-    anchor.href = '#';
-    anchor.dataset.sidebarNavTarget = i;
-    anchor.addEventListener('click', smoothScroll);
-    aside.append(anchor);
-    heading.dataset.sidebarNavId = i;
+    const smoothScroll = event => {
+      event.preventDefault();
+
+      const scrollTarget = content.querySelector(`[data-sidebar-nav-id='${event.target.dataset.sidebarNavTarget}']`);
+      jump(scrollTarget, {
+        offset: -120
+      });
+    };
+
+    headings.forEach((heading, index) => {
+      const anchor = document.createElement('a');
+      anchor.textContent = heading.textContent;
+      anchor.classList.add('link');
+      anchor.classList.add('sidebar-link');
+      anchor.href = '#';
+      anchor.dataset.sidebarNavTarget = index;
+      anchor.addEventListener('click', smoothScroll);
+      aside.append(anchor);
+      heading.dataset.sidebarNavId = index;
+      observer.observe(heading);
+    });
   });
 }
